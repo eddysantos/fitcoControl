33dfbd8be0ee52b9fdcb81f25fc7859bccec90b5
epinales <EstefaniaPinales@Estefanias-MacBook-Pro.local>
2017-10-24 17:22:50 -0500

    ajustes menu Bienvenida

--- Ubicaciones/Bienvenido.php
+++ Ubicaciones/Bienvenido.php
@@ -25,42 +25,66 @@
     <meta charset="utf-8">
     <title>Fit&amp;Co Solutions</title>
     <link rel="stylesheet" href="/fitcoControl/Resources/css/barranavegacion.css">
+    <link rel="stylesheet" href="/fitcoControl/Resources/bootstrap/css/bootstrap.min.css">
     <link rel='stylesheet prefetch' href='http://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css'>
+
+
   </head>
+  <style media="screen">
+    body{
+      background-color: rgb(66, 98, 119)!important;
+    }
+  </style>
   <body>
     <div id="bienvenida">
       <h1 id="logo">FIT&amp;CO</h1>
-      <nav>
-        <?php if ($pv == 1): ?>
+
+      <ul class="nav nav-pills nav-fill">
+      <?php if ($pv == 1): ?>
+        <li class="nav-item">
           <a href="/fitcoControl/Ubicaciones/Produccion/produccion.php" class="transicion">PRODUCCIÓN</a>
-        <?php else: ?>
+        </li>
+      <?php else: ?>
+        <li class="nav-item">
           <a class="bloqueo">PRODUCCION</a>
-        <?php endif; ?>
+        </li>
+      <?php endif; ?>
 
 
-        <?php if ($cv == 1): ?>
+      <?php if ($cv == 1): ?>
+        <li class="nav-item">
           <a href="/fitcoControl/Ubicaciones/Cobranza/cobranza.php" class="transicion">COBRANZA</a>
-        <?php else: ?>
+        </li>
+      <?php else: ?>
+        <li class="nav-item">
           <a class="bloqueo">COBRANZA</a>
-        <?php endif; ?>
+        </li>
+      <?php endif; ?>
 
-
-
-        <?php if ($clv == 1): ?>
+      <?php if ($cv == 1): ?>
+        <li class="nav-item">
           <a href="/fitcoControl/Ubicaciones/Clientes/Clientes.php" class="transicion">CLIENTES</a>
-        <?php else: ?>
+        </li>
+      <?php else: ?>
+        <li class="nav-item">
           <a class="bloqueo">CLIENTES</a>
-        <?php endif; ?>
+        </li>
+      <?php endif; ?>
 
-
-        <?php if ($admin): ?>
-            <a href="/fitcoControl/Ubicaciones/Usuarios/Usuarios.php" class="transicion">USUARIOS</a>
-        <?php else: ?>
+      <?php if ($cv == 1): ?>
+        <li class="nav-item">
+          <a href="/fitcoControl/Ubicaciones/Usuarios/Usuarios.php" class="transicion">USUARIOS</a>
+        </li>
+      <?php else: ?>
+        <li class="nav-item">
           <a class="bloqueo">USUARIOS</a>
-        <?php endif; ?>
+        </li>
+      <?php endif; ?>
 
-        <a href="/fitcoControl/Resources/PHP/Login/CerrarSesion.php" class="transicion">CERRAR SESION</a>
-      </nav>
+        <li class="nav-item">
+          <a href="/fitcoControl/Resources/PHP/Login/CerrarSesion.php" class="transicion">CERRAR SESION</a>
+        </li>
+      </ul>
     </div>
 
     <section id="homesec">
