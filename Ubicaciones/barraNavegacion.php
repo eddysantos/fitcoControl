f137d3a48d0b3292f8b5b243c66a0298b91d70da
Eduardo Santos <e.santosancira@gmail.com>
2017-10-24 15:57:10 -0500

    Privilegios
    Corrección de privilegios en barra de navegación.

--- Ubicaciones/barraNavegacion.php
+++ Ubicaciones/barraNavegacion.php
@@ -3,7 +3,7 @@
   $pe = $_SESSION['user']['produccion_editar'];
   $cv =  $_SESSION['user']['cobranza_ver'];
   $clv = $_SESSION['user']['cliente_ver'];
-  $admin = $_SESSION['user']['privilegiosUsuario'];
+  $admin = $_SESSION['user']['privilegiosUsuario'] == "Administrador";
 
   // $priv_produccion = $pv == "1" && $cv == "0" && $clv == "0";
   // $priv_clientes =  $clv == "1" && $pv == "0" && $cv == "0";
@@ -12,7 +12,7 @@
   // $priv_cp = $cv && $pv == "1" && $clv == "0";
   // $priv_ccl = $clv && $cv == "1" && $pv == "0";
   // $priv_todos = $clv && $cv && $pv == "1";
-  //
+
 
   $priv_todos = true;
 
@@ -59,57 +59,33 @@
     <nav class="nav_animate">
 
     <ul class="nav nav-pills nav-fill" id="selecTipoPoliza">
-      <?php if ($priv_produccion): ?>
-      <li class="nav-item"><a href="/fitcoControl/Ubicaciones/Produccion/produccion.php" class="w-95">PRODUCCION</a></li>
-      <li class="nav-item"><a class="bloqueo w-95">COBRANZA</a></li>
-      <li class="nav-item"><a class="bloqueo w-95">CLIENTES</a></li>
-      <li class="nav-item"><a class="bloqueo w-95">USUARIOS</a></li>
 
-    <?php elseif ($priv_cobranza):?>
-      <li class="nav-item"><a class="bloqueo w-95">PRODUCCION</a></li>
-      <li class="nav-item"><a href="/fitcoControl/Ubicaciones/Cobranza/cobranza.php" class="w-95">COBRANZA</a></li>
-      <li class="nav-item"><a class="bloqueo w-95">CLIENTES</a></li>
-      <li class="nav-item"><a class="bloqueo w-95">USUARIOS</a></li>
-
-
-    <?php elseif ($priv_clientes):?>
-      <li class="nav-item"><a class="bloqueo w-95">PRODUCCION</a></li>
-      <li class="nav-item"><a class="bloqueo w-95">COBRANZA</a></li>
-      <li class="nav-item"><a href="/fitcoControl/Ubicaciones/Clientes/Clientes.php" class="w-95">CLIENTES</a></li>
-      <li class="nav-item"><a class="bloqueo w-95">USUARIOS</a></li>
-
-    <?php elseif ($priv_pcl): ?>
-      <li class="nav-item"><a href="/fitcoControl/Ubicaciones/Produccion/produccion.php" class="w-95">PRODUCCION</a></li>
-      <li class="nav-item"><a class="bloqueo w-95">COBRANZA</a></li>
-      <li class="nav-item"><a href="/fitcoControl/Ubicaciones/Clientes/Clientes.php" class="w-95">CLIENTES</a></li>
-      <li class="nav-item"><a class="bloqueo w-95">USUARIOS</a></li>
-
-
-    <?php elseif ($priv_cp):?>
-      <li class="nav-item"><a href="/fitcoControl/Ubicaciones/Produccion/produccion.php" class="w-95">PRODUCCION</a></li>
-      <li class="nav-item"><a href="/fitcoControl/Ubicaciones/Cobranza/cobranza.php" class="w-95">COBRANZA</a></li>
-      <li class="nav-item"><a class="bloqueo w-95">CLIENTES</a></li>
-      <li class="nav-item"><a class="bloqueo w-95">USUARIOS</a></li>
-
-    <?php elseif ($priv_ccl): $ocultar = "ocultar";?>
-      <li class="nav-item"><a class="bloqueo w-95">PRODUCCION</a></li>
-      <li class="nav-item"><a href="/fitcoControl/Ubicaciones/Cobranza/cobranza.php" class="w-95">COBRANZA</a></li>
-      <li class="nav-item"><a href="/fitcoControl/Ubicaciones/Clientes/Clientes.php" class="w-95">CLIENTES</a></li>
-      <li class="nav-item"><a class="bloqueo w-95">USUARIOS</a></li>
-
-    <?php elseif ($priv_todos):?>
-      <li class="nav-item"><a href="/fitcoControl/Ubicaciones/Produccion/produccion.php" class="w-95">PRODUCCION</a></li>
-      <li class="nav-item"><a href="/fitcoControl/Ubicaciones/Cobranza/cobranza.php" class="w-95">COBRANZA</a></li>
-      <li class="nav-item"><a href="/fitcoControl/Ubicaciones/Clientes/Clientes.php" class="w-95">CLIENTES</a></li>
-      <li class="nav-item"><a href="/fitcoControl/Ubicaciones/Usuarios/Usuarios.php" class="w-95">USUARIOS</a></li>
-      <!--li class="nav-item"><a class="w-95">USUARIOS</a></li-->
-
-      <?php elseif ($admin == "Administrador"):?>
+      <?php if ($pv == 1): ?>
         <li class="nav-item"><a href="/fitcoControl/Ubicaciones/Produccion/produccion.php" class="w-95">PRODUCCION</a></li>
-        <li class="nav-item"><a href="/fitcoControl/Ubicaciones/Cobranza/cobranza.php" class="w-95">COBRANZA</a></li>
-        <li class="nav-item"><a href="/fitcoControl/Ubicaciones/Clientes/Clientes.php" class="w-95">CLIENTES</a></li>
-        <li class="nav-item"><a href="/fitcoControl/Ubicaciones/Usuarios/Usuarios.php" class="w-95">USUARIOS</a></li>
+      <?php else: ?>
+        <li class="nav-item"><a class="bloqueo w-95">PRODUCCION</a></li>
       <?php endif; ?>
+
+      <?php if ($cv == 1): ?>
+        <li class="nav-item"><a href="/fitcoControl/Ubicaciones/Cobranza/cobranza.php" class="w-95">COBRANZA</a></li>
+      <?php else: ?>
+        <li class="nav-item"><a class="bloqueo w-95">COBRANZA</a></li>
+      <?php endif; ?>
+
+      <?php if ($clv == 1): ?>
+        <li class="nav-item"><a href="/fitcoControl/Ubicaciones/Clientes/Clientes.php" class="w-95">CLIENTES</a></li>
+      <?php else: ?>
+        <li class="nav-item"><a class="bloqueo w-95">CLIENTES</a></li>
+      <?php endif; ?>
+
+      <?php if ($admin): ?>
+        <li class="nav-item"><a href="/fitcoControl/Ubicaciones/Usuarios/Usuarios.php" class="w-95">USUARIOS</a></li>
+      <?php else: ?>
+        <li class="nav-item"><a class="bloqueo w-95">USUARIOS</a></li>
+      <?php endif; ?>
+
+
+
       <li class="nav-item"><a href="/fitcoControl/Resources/PHP/Login/CerrarSesion.php" class="w-95">CERRAR SESION</a></li>
     </ul>
   </nav>
