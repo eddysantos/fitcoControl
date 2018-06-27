1ee026ed4b5011318911275cc4a04fe58a1e52f9
epinales <ae_pinales@hotmail.com>
2018-06-27 11:44:16 -0500

    Modal de DataTable y pagina principal

---
+++ Ubicaciones/Cobranza/Datatable/dataTable.php
@@ -0,0 +1,116 @@
+
+ <?php
+ // session_start();
+ //
+ // if (!isset($_SESSION['user'])) {
+ //   header("Location: /fitcoControl/index.php");
+ // }
+   $root = $_SERVER['DOCUMENT_ROOT'];
+   require $root . '/fitcoControl/Ubicaciones/barraNavegacion.php';
+   require $root . '/fitcoControl/Ubicaciones/Cobranza/Datatable/modalDataTable.php';
+   require $root . '/fitcoControl/Ubicaciones/Modales/Cobranza/ModalGraficaCobranza.php';
+   // require $root . '/fitcoControl/Ubicaciones/Modales/Cobranza/ModalPagos.php';
+
+
+
+ ?>
+
+ <!-- <div class="container-fluid pl-75 pr-57">
+   <div class="row clt_usr mt-4">
+     <div class="text-left alert alert-info w-65" role="alert" >
+       <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+       <strong>Nota: </strong> En esta sección se podran registrar las facturas que nos van a pagar, se podra editar en <img src='/fitcoControl/Resources/iconos/001-edit-1.svg' class='iconoNota'> y eliminar registro en <img src='/fitcoControl/Resources/iconos/004-delete-1.svg' class='iconoNota'>. Adicional se podra agregar un pago completo de la factura o un abono  en el icono <img src='/fitcoControl/Resources/iconos/003-add.svg' class='iconoNota'> y se podra ver el desglose de pagos que nos ha realizado el cliente en este icono <img src='/fitcoControl/Resources/iconos/magnifier.svg' class='iconoNota'>
+     </div>
+   </div>
+ </div> -->
+  <body>
+
+
+    <form id='Ecobranza' class="dt-page p-0" >
+      <div class="container-fluid mb-5">
+        <table id="db_cobranza" class=" table-hover text-center ">
+          <div class="row clt_usr mt-4">
+            <div class="col align-self-end">
+              <a class="rotate-link consultar ancla" accion="acobranza" status="cerrado">
+                <img  src="/fitcoControl/Resources/iconos/003-analytics-2.svg" class="icon rotate-icon" style="width:30px">
+                <span class="spanA">Agregar Factura</span>
+              </a>
+              <a class="rotate-link consultar ancla" data-toggle='modal' data-target='#ModalGraficaCobranza'>
+                <img  src="/fitcoControl/Resources/iconos/grafica2.svg" class=" icon rotate-icon" style="width:30px">
+                <span class="spanD">Graficas Cobranza</span>
+              </a>
+            </div>
+          </div>
+          <thead class="text-center">
+            <tr class="encabezado">
+              <td><p></p></td>
+              <td><p>Cliente</p></td>
+              <td><p>Concepto</p></td>
+              <td><p>Factura</p></td>
+              <td><p>Importe</p></td>
+              <td><p>Vencimiento</p></td>
+              <td><p></p></td>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </form>
+  </body>
+
+
+
+  <div class="modal fade" id="VisualizarTablaCobranza">
+    <div class="modal-dialog modal-med">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button class="close" type="button" name="button" data-dismiss="modal" area-label="close">
+            <i class="fa fa-times-circle-o fa-2x" aria-hidden="true"></i>
+          </button>
+          <h5 class="modal-tittle">Tabla de Pagos</h5>
+        </div>
+        <div class="modal-body">
+          <div class="container-fluid">
+            <table class="table">
+              <thead>
+                <tr class="row encabezado">
+                  <td class='col-md-5 text-center'>
+                    <h4><b>FECHA</h4>
+                  </td>
+                  <td class='col-md-5 text-center'>
+                    <h4><b>PAGADO</b></h4>
+                  </td>
+                  <td class='col-md-2 text-center'></td>
+                </tr>
+              </thead>
+              <tbody id="visualizarCobranza"></tbody>
+            </table>
+          </div><!--termina el Container-Fluid-->
+        </div><!--termina el Cuerpo del Modal-->
+      </div><!--termina el COntenido del Modal-->
+    </div>
+  </div>
+
+
+
+  <footer class="footer">
+    <li class="nav-item">
+      <a  class="bn noborder w-100" href="/fitcoControl/Resources/PHP/Login/CerrarSesion.php" >
+      <div class="row justify-content-center">
+        <div class="col-md-3">
+          Cerrar <img class="m-3" style="width:30px" src="/fitcoControl/Resources/iconos/001-close-1.svg"> Sesión</a>
+        </div>
+      </div>
+    </li>
+
+<?php
+  // require $root . '/fitcoControl/Resources/PHP/Cobranza/pieCobranza.php';
+?>
+
+  <script src="/fitcoControl/Resources/js/Cobranza/Datatable.js"></script>
+  <script src="/fitcoControl/Resources/js/MostrarDivsAgregar.js"></script>
+
+  <!-- <script src="/fitcoControl/Resources/js/Cobranza/Cobranza.js"></script> -->
+  <script src="/fitcoControl/Resources/js/Inputs.js"></script>
+
+
+  </footer>
---
+++ Ubicaciones/Cobranza/Datatable/modalDataTable.php
@@ -0,0 +1,192 @@
+<!--MODAL AGREGAR EN DATA TABLES  -->
+
+
+<form  id="Agregarcobranza" onsubmit="return false" class="agregarnuevo" style="display:none;margin-bottom:80px">
+  <table class="table">
+    <tbody>
+      <tr class="row m20">
+        <td class="col-md-12 input-effect p-0">
+          <input type="text" id="cbz_id" style="display:none">
+          <td class="col-md-12 input-effect p-0">
+            <input class="effect-17" type="text" id="npClientName" required autocomplete="off">
+              <label>Cliente</label>
+              <span class="focus-border"></span>
+              <div class="client-list" id="npClientList" style="display: none">
+              </div>
+          </td>
+        </td>
+      </tr>
+      <tr class="row m20">
+        <td class="col-md-12 input-effect p-0">
+          <input id="cbz_concepto" class="w-100 effect-17" list="conc" required>
+          <datalist id="conc">
+            <option value="Maquila">Maquila</option>
+            <option value="Bordado">Bordado</option>
+            <option value="Flete">Flete</option>
+          </datalist>
+          <label>Concepto</label>
+          <span class="focus-border"></span>
+        </td>
+      </tr>
+      <tr class="row m20">
+        <td class="col-md-12 input-effect p-0">
+          <input id="cbz_factura" class="effect-17" type="text" required>
+            <label>No. Factura</label>
+            <span class="focus-border"></span>
+        </td>
+      </tr>
+      <tr class="row m20">
+        <td class="col-md-12 input-effect p-0">
+          <input id="cbz_importe" class="effect-17" type="text" required>
+            <label>$ Importe</label>
+            <span class="focus-border"></span>
+        </td>
+      </tr>
+      <tr class="row m20">
+        <td class="col-md-12 input-effect p-0">
+          <input id="cbz_entrega" class="effect-17 has-content" type="date" required>
+            <label>Fecha de Entrega</label>
+            <span class="focus-border"></span>
+        </td>
+      </tr>
+      <tr class="row m20">
+        <td class="col-md-12 input-effect p-0">
+          <input id="cbz_dvencimiento" class="effect-17 has-content" type="date" required>
+            <label>Vencimiento de factura</label>
+            <span class="focus-border"></span>
+        </td>
+      </tr>
+      <tr class="row justify-content-center m-0 mb-2 mt-5">
+        <td class="col-md-4">
+          <button type="submit" id="NuevoRegistroCobranza" class="btnsub btn boton btn-block ">AGREGAR</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</form>
+
+
+
+<!--MODAL EDITAR EN DATA TABLES  -->
+<?php
+// $root = $_SERVER['DOCUMENT_ROOT'];
+// require $root . '/fitcoControl/Resources/PHP/DataBases/ConexionDataTables.php';
+// $cn = getConexion();
+// $sql = "SELECT *  FROM ct_cobranza";
+// $res = $cn->query($sql);
+
+// $id = $POST['pk_cobranza'];
+?>
+
+<div class="modal fade" id="editarCobranza">
+  <div class="modal-dialog modal-med">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button class="close" type="button" name="button" data-dismiss="modal" area-label="close">
+          <i class="fa fa-times-circle-o fa-2x" aria-hidden="true"></i>
+        </button>
+        <h5 class="modal-tittle">EDITAR</h5>
+      </div>
+      <div class="modal-body">
+        <table class="table">
+          <tbody>
+            <tr class="row m20">
+              <td class="col-md-12 input-effect p-0">
+                <input type="hidden" name="mcbz_id" id="mcbz_id">
+                <input class="modal-efecto-17 has-content w-100" name="mcbz_cliente" id="mcbz_cliente" required autocomplete="off" client-id="">
+
+                <span class="focus-border"></span>
+                <div class="client-list" id="mcbz_ClientList" style="display: none">
+                </div>
+              </td>
+            </tr>
+            <tr class="row m20">
+              <td class="col-md-12 input-effect p-0">
+                <input name="mcbz_concepto" id="mcbz_concepto" class="modal-efecto-17 has-content w-100" list="conc" required>
+                <datalist id="conc">
+                  <option value="Maquila">Maquila</option>
+                  <option value="Bordado">Bordado</option>
+                  <option value="Flete">Flete</option>
+                </datalist>
+                <label>Concepto</label>
+                <span class="focus-border"></span>
+              </td>
+            </tr>
+            <tr class="row m20">
+              <td class="col-md-12 input-effect p-0">
+                <input class="modal-efecto-17 has-content" name="mcbz_factura" id="mcbz_factura" type="text">
+                  <label>Factura</label>
+                  <span class="focus-border"></span>
+              </td>
+            </tr>
+            <tr class="row m20">
+              <td class="col-md-12 input-effect p-0">
+                <input class="modal-efecto-17 has-content" name="mcbz_importe" id="mcbz_importe" type="text">
+                  <label>Importe</label>
+                  <span class="focus-border"></span>
+              </td>
+            </tr>
+            <tr class="row m20">
+              <td class="col-md-12 input-effect p-0">
+                <input class="modal-efecto-17 has-content" name="mcbz_entrega" id="mcbz_entrega" type="date">
+                  <label>Fecha Entrega</label>
+                  <span class="focus-border"></span>
+              </td>
+            </tr>
+            <tr class="row m20">
+              <td class="col-md-12 input-effect p-0">
+                <input class="modal-efecto-17 has-content" name="mcbz_vencimiento" id="mcbz_vencimiento" type="date">
+                  <label>Día Vencimiento</label>
+                  <span class="focus-border"></span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div><!--termina el Cuerpo del Modal-->
+      <div class="justify-content-center modal-footer">
+        <button type="submit"  class="ActualizarDcobranza w-50 btnsub btn boton btn-block ">ACTUALIZAR</button>
+      </div>
+    </div><!--termina el COntenido del Modal-->
+  </div>
+</div>
+
+
+
+
+<!-- AGREGAR PAGO  -->
+<div class="modal fade" id="PagoFacturas">
+  <div class="modal-dialog modal-med">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button class="close" type="button" name="button" data-dismiss="modal" area-label="close">
+          <i class="fa fa-times-circle-o fa-2x" aria-hidden="true"></i>
+        </button>
+        <h5 class="modal-tittle">AGREGAR PAGO</h5>
+      </div>
+      <div class="modal-body">
+        <table class="table">
+          <tbody>
+            <tr class="row m20">
+              <td class="col-md-12 input-effect p-0">
+                <input type="hidden" name="mpgo_id" id="mpgo_id">
+                <input class="modal-efecto-17 has-content" name="mpgo_fpago" id="mpgo_fpago" type="date">
+                  <label>Fecha Pago</label>
+                  <span class="focus-border"></span>
+              </td>
+            </tr>
+            <tr class="row m20">
+              <td class="col-md-12 input-effect p-0">
+                <input class="modal-efecto-17 has-content" name="mpgo_importe" id="mpgo_importe" type="text">
+                  <label>Importe</label>
+                  <span class="focus-border"></span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div><!--termina el Cuerpo del Modal-->
+      <div class="justify-content-center modal-footer">
+        <button  id="AgregarPgo" type="submit" class="AgregarPgo w-50 btnsub btn boton btn-block ">AGREGAR</button>
+      </div>
+    </div><!--termina el COntenido del Modal-->
+  </div>
+</div>
